985ba6d8cb9e07c5315989b19d225a64f5334914
Vitalii A <crazyyy@gmail.com>
2025-05-02 15:12:46 +0300

    Upd config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/crazyyy/crazyyy.github.io', // ← назва репозиторію
+  base: '/resume-site/', // ← назва репозиторію
   plugins: [react()],
 })
